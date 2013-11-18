fcc1b8d13ecfe666070642a271d766eb3f475381
Jeff Cohen <jeff@starterleague.com>
2013-11-18 11:50:44 -0600

    has_many :through

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,9 +2,9 @@ class Movie < ActiveRecord::Base
 
   validates_presence_of :title
 
-  # validates :title, :presence => true
-
   has_many :roles, :class_name => "Role", :foreign_key => :movie_id
+  has_many :actors, :through => :roles
+
   has_many :reviews, :class_name => "Review", :foreign_key => :movie_id
   belongs_to :director, :class_name => "Director", :foreign_key => :director_id
 
